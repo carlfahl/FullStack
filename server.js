530a2049e6ea8a7822e5adb1b685eb2e0b12e4eb
Carl A. Fahlstrom <carl.fahlstrom@gmail.com>
2017-02-07 20:21:38 -0700

    Made express app with basic api endpoints.

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 // Require the dependencies
 
-var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end("Hello World!!  I am Sharknato!!!");
-}).listen(3000);
+var app = express();
+
+// Let us read Post Data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send("Hello World!!!  I am Sharknato!!!");
+});
+
+app.get('/api/greeting', function (req, res) {
+  res.send('Hello.  Welcome to the greeting API');
+});
+
+app.post('/api/greeting', function (req, res) {
+  var name = req.body.name;
+  var greeting = req.body.greeting;
+  res.json({name: name, greeting: greeting});
+});
+
+app.get('/api/greeting/:name', function (req, res) {
+  res.send("Hello, "+ req.params.name);
+});
+
+app.listen(3000);
 
 console.log('Server started on Port 3000.')
